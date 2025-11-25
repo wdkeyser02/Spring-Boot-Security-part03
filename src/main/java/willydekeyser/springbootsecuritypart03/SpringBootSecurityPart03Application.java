1bb80ec10de9fe87f3dd7ace293770509ab09321
willydekeyser <ma074172@skynet.be>
2025-11-25 12:18:26 +0100

    Initial commit

---
+++ src/main/java/willydekeyser/springbootsecuritypart03/SpringBootSecurityPart03Application.java
@@ -0,0 +1,13 @@
+package willydekeyser.springbootsecuritypart03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityPart03Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityPart03Application.class, args);
+    }
+
+}
